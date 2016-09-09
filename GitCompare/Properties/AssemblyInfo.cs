74e78e6a4303dd43f955ac71faf031d95cfd18d4
Kevin Phelps <KevinPhelps11@gmail.com>
2016-09-08 22:06:20 -0500

    remove assembly version, forgot it was in project.json

--- GitCompare/Properties/AssemblyInfo.cs
+++ GitCompare/Properties/AssemblyInfo.cs
@@ -10,7 +10,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("GitCompare")]
 [assembly: AssemblyCopyright("Copyright 2016 Kevin Phelps")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyVersion("1.2.0.0")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
